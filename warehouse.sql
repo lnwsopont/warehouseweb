8a3e0758e21113bac117aca032b6dadacce48daa
Sopon <sopontrangansri@gmail.com>
2017-09-13 11:33:54 +0700

    no message

---
+++ warehouse.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 13, 2017 at 04:31 AM
+-- Server version: 5.7.17-log
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `warehouse`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `contact_id` int(11) NOT NULL,
+  `contact_name` varchar(40) NOT NULL,
+  `contact_detail` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery_info`
+--
+
+CREATE TABLE `delivery_info` (
+  `orders_id` int(11) NOT NULL,
+  `delivery_id` int(11) NOT NULL,
+  `emp_id` int(11) NOT NULL,
+  `transport_id` int(11) NOT NULL,
+  `delivery_status` int(11) NOT NULL COMMENT '1= inshelf 2=packing 3=loading 4=delivering 5=delivered 0=cancel',
+  `delivery_datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `emp_id` int(11) NOT NULL,
+  `emp_fname` varchar(40) NOT NULL,
+  `emp_lname` varchar(40) NOT NULL,
+  `emp_status` int(11) NOT NULL COMMENT 'hire=1 inactive=0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orders_id` int(11) NOT NULL,
+  `order_datetime` datetime NOT NULL,
+  `emp_id` int(11) NOT NULL,
+  `contact_id` int(11) NOT NULL,
+  `orders_action` varchar(6) NOT NULL COMMENT 'in or out',
+  `orders_status` int(11) NOT NULL DEFAULT '1' COMMENT '1=active 0=inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_detail`
+--
+
+CREATE TABLE `orders_detail` (
+  `orders_id` int(11) NOT NULL,
+  `parcel_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parcel`
+--
+
+CREATE TABLE `parcel` (
+  `parcel_id` int(11) NOT NULL,
+  `parcel_name` varchar(300) NOT NULL,
+  `parcel_quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parcel_log`
+--
+
+CREATE TABLE `parcel_log` (
+  `parcellog_id` int(11) NOT NULL,
+  `shelf_code` int(11) NOT NULL,
+  `parcel_id` int(11) NOT NULL,
+  `check_date` datetime NOT NULL,
+  `check_action` varchar(6) NOT NULL COMMENT 'in or out',
+  `emp_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shelf`
+--
+
+CREATE TABLE `shelf` (
+  `shelf_code` varchar(5) NOT NULL,
+  `shelf_capacity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transport`
+--
+
+CREATE TABLE `transport` (
+  `transport_plate` varchar(9) NOT NULL,
+  `transport_brand` int(11) NOT NULL,
+  `transport_model` int(11) NOT NULL,
+  `transport_size` varchar(2) NOT NULL COMMENT 'S,M,L'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`contact_id`);
+
+--
+-- Indexes for table `delivery_info`
+--
+ALTER TABLE `delivery_info`
+  ADD PRIMARY KEY (`orders_id`,`delivery_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orders_id`);
+
+--
+-- Indexes for table `orders_detail`
+--
+ALTER TABLE `orders_detail`
+  ADD PRIMARY KEY (`orders_id`,`parcel_id`);
+
+--
+-- Indexes for table `parcel`
+--
+ALTER TABLE `parcel`
+  ADD PRIMARY KEY (`parcel_id`);
+
+--
+-- Indexes for table `parcel_log`
+--
+ALTER TABLE `parcel_log`
+  ADD PRIMARY KEY (`parcellog_id`);
+
+--
+-- Indexes for table `shelf`
+--
+ALTER TABLE `shelf`
+  ADD PRIMARY KEY (`shelf_code`);
+
+--
+-- Indexes for table `transport`
+--
+ALTER TABLE `transport`
+  ADD PRIMARY KEY (`transport_plate`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `parcel`
+--
+ALTER TABLE `parcel`
+  MODIFY `parcel_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `parcel_log`
+--
+ALTER TABLE `parcel_log`
+  MODIFY `parcellog_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
