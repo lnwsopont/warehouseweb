9ef69466fd98f3ea98ceb1af32497b2f9a339772
Sopon <sopontrangansri@gmail.com>
2017-11-20 18:09:37 +0700

    no message

--- views/employee/edit_parcel.php
+++ views/employee/edit_parcel.php
@@ -22,36 +22,33 @@
         <div class ="row">
 
             <div class="col s12">
-                
+
                 <ul class="collection">
                     <? foreach ($parcel as $parcel): ?>
-                    <li class="collection-item avatar">
-                        <img src="images/yuna.jpg" alt="" class="circle">  
-                        <p>
-                           ID: <? echo $parcel['parcel_id'] ?><br>
-                           
-                           <? if($parcel['parcel_status']== 0){?>
-                               Status: <? echo "Waiting for parcel";
-                           }
-                          else if($parcel['parcel_status']==1){ ?>
-                               Status: <? echo "Checking Into Shelf:" + $parcel['shelf_code'];
-                           }
-                            else if($parcel['parcel_status']==2){ ?>
-                               Status: <? echo "In-Shelf:" + $parcel['shelf_code'];
-                           }
-                           else{?>
-                               Status:<? echo"Checked Out";
-                           }?>
-                               <br>Customer ID: <? echo $parcel['cus_id'];?><br>
-                               Customer ID: <? echo $parcel['cus_id'];?><br>
-                               Parcel In Date: <? echo $parcel['parcel_indate'];?><br>
-                               Employee in(ID): <? echo $parcel['emp_id_in'];?><br>
-                        </p>
-                     
-                            <? endforeach; ?>
+                        <li class="collection-item avatar">
+                            <img src="images/yuna.jpg" alt="" class="circle">  
+                            <p>
+                                ID: <? echo $parcel['parcel_id'] ?><br> 
+                                <? if ($parcel['parcel_status'] == 0) { ?> 
+                                    <? echo " Waiting for parcel";
+                                } 
+                                elseif ($parcel['parcel_status'] == 1) {?>
+                                    <? echo "Status: Checking Into Shelf: " . $parcel['shelf_code'];} 
+                                elseif ($parcel['parcel_status'] == 2) {?> 
+                                    <? echo " Status: In-Shelf: " . $parcel['shelf_code'];
+                                } else {?>
+                                    <? echo"Status: Checked Out";
+                                        }?>
+                                <br>Customer ID: <? echo $parcel['cus_id']; ?><br>
+                                Customer ID: <? echo $parcel['cus_id']; ?><br>
+                                Parcel In Date: <? echo $parcel['parcel_indate']; ?><br>
+                                Employee in(ID): <? echo $parcel['emp_id_in']; ?><br>
+                            </p>
+
+<? endforeach; ?>
 
                         </div>
-                      
+
 
 
                         </div>
