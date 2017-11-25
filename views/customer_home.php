76764198b62c424ee82817509a8a0f42d477d717
KAVINNABATRA\Queeny <vjsabks>
2017-11-25 19:23:39 +0700

    no message

--- views/customer_home.php
+++ views/customer_home.php
@@ -20,29 +20,35 @@
     </div>
 
     <div class="col s6">
+        <a href="/history">
         <div class="card  light-green accent-2">
             <div class="card-content center">
                 <span class="card-title">Past Parcel</span>
                 <p style="font-size: 48px;"><?=$past_parcel; ?></p>
             </div>
         </div>
+        </a>
     </div>
 
     <div class="col s6">
+        <a href="/product">
         <div class="card yellow lighten-1">
             <div class="card-content center">
                 <span class="card-title">Booking Parcel</span>
                 <p style="font-size: 48px;"><?=$booking_parcel; ?></p>
             </div>
         </div>
+        </a>
     </div>
     <div class="col s6">
+        <a href="/product">
         <div class="card deep-orange accent-2">
             <div class="card-content center">
                 <span class="card-title">Pending Fee</span>
                 <p style="font-size: 48px;"><?= number_format($pending_fee); ?> ฿</p>
             </div>
-        </div>
+        </div
+        </a>
     </div>
 </div>
 
